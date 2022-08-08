e5f4e37b27fe74202d2ccd48674aca57fde3c9d6
L1Q <6942070+L1Q@users.noreply.github.com>
2022-08-08 17:18:09 +0300

    Rename instaed of repacking

--- util/repack_latest.sh
+++ util/repack_latest.sh
@@ -10,32 +10,22 @@ RELEASE_DIR="$(realpath "$1")"
 shift
 mkdir -p "$RELEASE_DIR"
 
-rm -rf repack_dir
-mkdir repack_dir
-trap "rm -rf repack_dir" EXIT
-
 while [[ $# -gt 0 ]]; do
     INPUT="$1"
     shift
 
     (
         set -e
-        REPACK_DIR="repack_dir/$BASHPID"
-        rm -rf "$REPACK_DIR"
-        mkdir "$REPACK_DIR"
 
         if [[ $INPUT == *.zip ]]; then
-            unzip "$INPUT" -d "$REPACK_DIR"
+            INAME=$(basename -s .zip -- "$INPUT")
         elif [[ $INPUT == *.tar.xz ]]; then
-            tar xvaf "$INPUT" -C "$REPACK_DIR"
+            INAME=$(basename -s .tar.xz -- "$INPUT")
         else
             echo "Unknown input file type: $INPUT"
             exit 1
         fi
 
-        cd "$REPACK_DIR"
-
-        INAME="$(echo ffmpeg-*)"
         TAGNAME="$(cut -d- -f2 <<<"$INAME")"
 
         if [[ $TAGNAME == N ]]; then
@@ -50,15 +40,11 @@ while [[ $# -gt 0 ]]; do
             ONAME="ffmpeg-$TAGNAME-latest-$(cut -d- -f3- <<<"$INAME")"
         fi
 
-        mv "$INAME" "$ONAME"
-
         if [[ $INPUT == *.zip ]]; then
-            zip -9 -r "$RELEASE_DIR/$ONAME.zip" "$ONAME"
+            cp $INPUT "$RELEASE_DIR/$ONAME.zip"
         elif [[ $INPUT == *.tar.xz ]]; then
-            tar cvJf "$RELEASE_DIR/$ONAME.tar.xz" "$ONAME"
+            cp $INPUT "$RELEASE_DIR/$ONAME.tar.xz"
         fi
-
-        rm -rf "$REPACK_DIR"
     ) &
 
     while [[ $(jobs | wc -l) -gt 3 ]]; do
@@ -69,4 +55,4 @@ done
 while [[ $(jobs | wc -l) -gt 0 ]]; do
     wait %1
 done
-rm -rf repack_dir
+
