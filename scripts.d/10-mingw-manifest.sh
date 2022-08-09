4bf1b182dee3dea8834ee0af4668f1524768d059
L1Q <6942070+L1Q@users.noreply.github.com>
2022-08-09 10:08:25 +0300

    Add manifest

---
+++ scripts.d/10-mingw-manifest.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+MANIFEST_OPATH_DIR="${FFBUILD_PREFIX}/lib/ffmpeg.exe.manifest"
+MANIFEST_OPATH="${MANIFEST_OPATH_DIR}/manifest.o"
+
+ffbuild_enabled() {
+    [[ $TARGET == win* ]] || return -1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    mkdir -p $MANIFEST_OPATH_DIR
+    mkdir manifest-build && cd manifest-build
+
+cat <<EOF >"./ffmpeg.exe.manifest.rc"
+1 24 "ffmpeg.exe.manifest"
+EOF
+
+cat <<EOF >"./ffmpeg.exe.manifest"
+<?xml version="1.0" encoding="utf-8"?>
+<assembly manifestVersion="1.0" xmlns="urn:schemas-microsoft-com:asm.v1">
+  <assemblyIdentity version="5.1.0.0" name="FFmpeg"/>
+  <application xmlns="urn:schemas-microsoft-com:asm.v3">
+    <windowsSettings>
+      <dpiAware xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true</dpiAware>
+    </windowsSettings>
+  </application>
+</assembly>
+EOF
+
+    $RC_COMPILER ./ffmpeg.exe.manifest.rc $MANIFEST_OPATH
+
+    cd ..
+    rm -rf ./manifest-build
+}
+
+ffbuild_libs() {
+  return $MANIFEST_OPATH
+}
